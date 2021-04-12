d6a31d61bf7ab15be24d700c1742c736de9ce336
alexagh26@gmail.com <Indigo0801>
2021-04-12 11:50:46 -0500

    changes

--- S EXAMPLE/class/person.js
+++ S EXAMPLE/class/person.js
@@ -1,6 +1,10 @@
-class person {
+ class person {
 
+     getInformation = () => {
+         let valueName = document.getElementById("input-name").value;
+         console.log(valueName)
 
+     }
 
-
-}
+ }
+ export default person;
---
+++ S EXAMPLE/index.js
@@ -0,0 +1,1 @@
+import person from './class/person';
