26f8b055741f1abd415027da3cc542b9ae6fb4bb
alexagh26@gmail.com <Indigo0801>
2021-04-12 10:47:58 -0500

    main structure

---
+++ S EXAMPLE/class/person.js
@@ -0,0 +1,6 @@
+class person {
+
+
+
+
+}
---
+++ S EXAMPLE/description_example.js
@@ -0,0 +1,8 @@
+/************* S — Single responsibility principle ********************/
+
+/* Este principio se refiere a la responsabilidad Única que debiera 
+tener cada programa con una tarea bien específica. */
+
+
+/* En este caso vamos a hacer un pequeño sistema que calcule el indice de masa muscular
+   cuando el usuario ingresa su péso y debe arrogarle el resultado. */
